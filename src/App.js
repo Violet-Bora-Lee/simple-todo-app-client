9188219f837c8753bd1ca416a3b681383c5afb3e
Violet Bora Lee <learnerbr@gmail.com>
2020-04-21 18:56:08 +0900

    Alert 컴포넌트, context 구현

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Alerts from "./components/layout/Alerts";
 import TodoState from './context/todo/TodoState';
 import './App.css';
+import AlertState from "./context/alert/alertState";
 
 const App = () => {
 	return (
 		<TodoState>
-			<Router>
-				<Fragment>
-					<Navbar/>
-					<div className="container">
-						<Switch>
-							<Route exact path='/' component={Home}/>
-							<Route exact path='/about' component={About}/>
-						</Switch>
-					</div>
-				</Fragment>
-			</Router>
+			<AlertState>
+				<Router>
+					<Fragment>
+						<Navbar/>
+						<div className="container">
+							<Alerts/>
+							<Switch>
+								<Route exact path='/' component={Home}/>
+								<Route exact path='/about' component={About}/>
+							</Switch>
+						</div>
+					</Fragment>
+				</Router>
+			</AlertState>
 		</TodoState>
 );
 }
---
+++ src/context/alert/alertState.js
@@ -0,0 +1,42 @@
+import React, { useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import AlertContext from './alertContext';
+import alertReducer from "./alertReducer";
+import {
+	SET_ALERT,
+	REMOVE_ALERT
+} from "../types";
+
+const AlertState = props => {
+	const initialState = [];
+
+	const [state, dispatch] = useReducer(alertReducer, initialState);
+
+	// 알람 설정
+	const setAlert = (msg, type, timeout = 3000) => {
+		const id = uuidv4();
+
+		dispatch({
+			type: SET_ALERT,
+			payload: { msg,	type, id }
+		});
+
+		setTimeout(() => dispatch({
+			type: REMOVE_ALERT,
+			payload: id
+		}), timeout)
+	};
+
+	return (
+		<AlertContext.Provider
+		  value={{
+		  	alerts: state,
+				setAlert
+			}}
+		>
+			{props.children}
+		</AlertContext.Provider>
+	);
+};
+
+export default AlertState;
