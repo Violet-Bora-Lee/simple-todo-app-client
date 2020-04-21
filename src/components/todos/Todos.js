b487e87f68234cd7ce1a8d24c440d64bb01086ab
Violet Bora Lee <learnerbr@gmail.com>
2020-04-22 03:24:49 +0900

    Spinner 컴포넌트

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -1,25 +1,31 @@
 import React, { Fragment, useContext, useEffect } from 'react';
 import TodoItem from './TodoItem';
 import TodoContext from '../../context/todo/todoContext';
+import Spinner from '../layout/Spinner';
 
 const Todos = () => {
 	const todoContext = useContext(TodoContext);
 
-	const { todos, getTodos } = todoContext;
+	const { todos, getTodos, loading } = todoContext;
 
 	useEffect(() => {
 		getTodos();
 	}, [])
 
-	if (todos !== null && todos.length === 0) {
+	if (todos !== null && todos.length === 0 && !loading) {
 		return <h4>해야할 일이 없습니다.</h4>
 	}
 
 	return (
 		<Fragment>
-			{todos && todos.map(todo =>
-			  <TodoItem key={todo._id} todo={todo}/>
-			)}
+			{todos !== null && !loading ? (
+					<Fragment>
+						{todos && todos.map(todo =>
+							<TodoItem key={todo._id} todo={todo}/>
+						)}
+					</Fragment>
+				)
+				: (<Spinner/>)}
 		</Fragment>
 	)
 }
--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -14,22 +14,26 @@ export default (state, action) => {
 		case GET_TODOS:
 			return {
 				...state,
-				todos: action.payload
+				todos: action.payload,
+				loading: false
 			}
 		case ADD_TODO:
 			return {
 				...state,
-				todos: [action.payload, ...state.todos]
+				todos: [action.payload, ...state.todos],
+				loading: false
 			}
 		case UPDATE_TODO:
 			return {
 				...state,
-				todos: state.todos.map(todo => todo.id === action.payload.id ? action.payload: todo)
+				todos: state.todos.map(todo => todo.id === action.payload.id ? action.payload: todo),
+				loading: false
 			}
 		case DELETE_TODO:
 			return {
 				...state,
-				todos: state.todos.filter(todo => todo.id !== action.payload)
+				todos: state.todos.filter(todo => todo.id !== action.payload),
+				loading: false
 			}
 		case SET_CURRENT:
 			return {
